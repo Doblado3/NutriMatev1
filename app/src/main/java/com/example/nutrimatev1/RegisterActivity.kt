d04c1944faf22eb4392e048c4d9fd21b421de4c5
Doblado3 <Pablodoza11@gmail.com>
2024-12-29 15:42:25 +0100

    Registro Completado

--- app/src/main/java/com/example/nutrimatev1/RegisterActivity.kt
+++ app/src/main/java/com/example/nutrimatev1/RegisterActivity.kt
@@ -16,10 +16,12 @@ import com.example.nutrimatev1.administrador.MainAdministrador
 import com.example.nutrimatev1.medico.MainMedico
 import com.example.nutrimatev1.modelo.Alert
 import com.example.nutrimatev1.paciente.MainPaciente
+import com.google.firebase.Timestamp
 import com.google.firebase.auth.FirebaseAuthException
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
+import java.text.SimpleDateFormat
 
 class RegisterActivity : AppCompatActivity() {
 
@@ -29,6 +31,10 @@ class RegisterActivity : AppCompatActivity() {
     private lateinit var textRegisterSex: TextView
     private lateinit var RradioGroupSexo: RadioGroup
     private lateinit var btnRegister: Button
+
+    private lateinit var telefono: EditText
+    private lateinit var fechaNac: EditText
+    private lateinit var apellidos: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,6 +47,10 @@ class RegisterActivity : AppCompatActivity() {
         RradioGroupSexo = findViewById(R.id.RradioGroupSexo)
         btnRegister = findViewById(R.id.btnRegister)
 
+        telefono = findViewById(R.id.ReditTextPhone)
+        fechaNac = findViewById(R.id.ReditTextDate)
+        apellidos = findViewById(R.id.ReditTextApellidos)
+
 
 
         RradioGroupSexo.setOnCheckedChangeListener { group, checkedId ->
@@ -50,30 +60,44 @@ class RegisterActivity : AppCompatActivity() {
 
         btnRegister.setOnClickListener {
             if (email.text.isNotBlank() && password.text.isNotBlank() && name.text.isNotBlank()) {
-                    val selectedSexoId = RradioGroupSexo.checkedRadioButtonId
-                    if (selectedSexoId != -1) {
-                        val selectedSexo = findViewById<RadioButton>(selectedSexoId).text.toString()
+                val selectedSexoId = RradioGroupSexo.checkedRadioButtonId
 
-
-                        registerUser(email.text.toString(), password.text.toString(), name.text.toString(),  selectedSexo)
-                    } else {
-                        Alert.showAlert(this,"Por favor, selecciona un sexo")
-                    }
-                } else {
-                    Alert.showAlert(this,"Por favor, selecciona un rol válido")
+                val e1 = Regex("^(\\+34|0034|34)?[67]\\d{8}\$")
+                val ok1 = e1.matches(telefono.text.toString())
+                if (!ok1) {
+                    Log.e("ERROR", "El teléfono introducido no es correcto")
+                    telefono.error = "Teléfono Incorrecto"
                 }
+
+                val e2 = Regex("^([0-2][0-9]|3[0-1])(\\/|-)(0[1-9]|1[0-2])\\2(\\d{4})\$")
+                val ok2 = e2.matches(fechaNac.text.toString())
+                if (!ok2) {
+                    Log.e("ERROR", "La fecha introducida no es correcta")
+                    fechaNac.error = "Fecha Incorrecta"
+                }
+
+                if (ok1 && ok2 && selectedSexoId != -1) {
+                    val selectedSexo = findViewById<RadioButton>(selectedSexoId).text.toString()
+                    val fecha = Timestamp(SimpleDateFormat("dd/MM/yyyy").parse(fechaNac.text.toString()))
+
+                    registerUser(email.text.toString(), password.text.toString(), name.text.toString(), selectedSexo,
+                        apellidos.text.toString(), fecha, telefono.text.toString())
+                }
+            } else {
+                Alert.showAlert(this,"Por favor, rellena todos los campos")
+            }
         }
     }
 
 
-    private fun registerUser(email: String, password: String, name: String, sexo: String) {
-        // Define the regex for doctors' emails (adjust the pattern as needed)
+    private fun registerUser(email: String, password: String, name: String, sexo: String, apellidos: String,
+                             fecha: Timestamp, telefono: String) {
+        // Tampoco necesitamos patrones demasiado complejos, fáciles de recordar
         val doctorEmailRegex = Regex("^[a-zA-Z0-9._%+-]+@hospital\\.com\$")
 
 
         val adminEmailRegex = Regex("^[a-zA-Z0-9._%+-]+@adm\\.com\$")
 
-        // Determine role based on email
         val role = when {
             doctorEmailRegex.matches(email) -> "Médico"
             adminEmailRegex.matches(email) -> "Administrativo"
@@ -84,7 +108,7 @@ class RegisterActivity : AppCompatActivity() {
             .addOnCompleteListener { task ->
                 if (task.isSuccessful) {
                     Log.i("INFO", "El usuario se ha registrado correctamente")
-                    saveUserToFirestore(email, name, role, sexo)
+                    saveUserToFirestore(email, name, role, sexo, apellidos, fecha, telefono)
                 } else {
                     val exception = task.exception
                     if (exception is FirebaseAuthException) {
@@ -96,12 +120,17 @@ class RegisterActivity : AppCompatActivity() {
             }
     }
 
-    private fun saveUserToFirestore(email: String, name: String, role: String, sexo: String) {
+    private fun saveUserToFirestore(email: String, name: String, role: String, sexo: String,
+                                    apellidos: String,
+                                    fecha: Timestamp, telefono: String) {
         val user = hashMapOf(
             "email" to email,
             "name" to name,
             "role" to role,
             "sexo" to sexo,
+            "apellidos" to apellidos,
+            "telefono" to telefono,
+            "fecha de nacimiento" to fecha,
         )
 
         val collection = when (role) {
@@ -110,6 +139,7 @@ class RegisterActivity : AppCompatActivity() {
             else -> "Pacientes"
         }
 
+        // Fijaos que es aquí donde se decide a donde redirigimos al usuario
         FirebaseFirestore.getInstance().collection(collection).document(email).set(user)
             .addOnSuccessListener {
                 Log.i("INFO", "Usuario guardado en Firestore")
@@ -119,6 +149,8 @@ class RegisterActivity : AppCompatActivity() {
                     else -> Intent(this, MainPaciente::class.java)
                 }
                 startActivity(intent)
+
+                // El finish() es para que no podamos volver hacia atrás de forma indebida
                 finish()
             }
             .addOnFailureListener {
